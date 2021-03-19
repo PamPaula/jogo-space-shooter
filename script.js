9b9373f5b2b3e8468303fbfcbf24c2a299154c09
PamPaula <pamela.dpaulas@gmail.com>
2021-03-19 15:46:50 -0300

    subindo o heroi

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function flyShip(event) {
         fireLaser();
     }
 }
+
+//subir o heroi
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "0px") {
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
